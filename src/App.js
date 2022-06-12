b15ddaf627d13bf39878fed5915dbc888b4d6250
Nicholas-Nguyen8742 <NicholasNguyen8742@gmail.com>
2022-06-12 03:57:02 -0400

    Imported instructions section into App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./styles/App.scss";
 import React from "react";
 import Header from "./components/Header/Header";
+import InstructionsSection from "./components/InstructionsSection/InstructionsSection";
 
 export default function App() {
   return (
     <div className="container">
       <Header />
-      <main></main>
+      <InstructionsSection />
     </div>
   );
 }
