901d1c2b37926309fef6d3e8a044d6db97f51d95
Nicholas-Nguyen8742 <NicholasNguyen8742@gmail.com>
2022-06-12 04:08:01 -0400

    Changed game title to logo to match ios app

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,18 @@
 import './Header.scss';
 import React from 'react';
+import logo from '../../assets/images/2048_logo.png';
+import InstructionsSection from '../InstructionsSection/InstructionsSection';
 
 
 export default function Header() {
     return (
         <header className='header'>
-            <h1 className='header__title'>2048</h1>
-            <article className='header__scores'>
+            <img className="header__logo" src={logo} alt='The Number 2048 on Yellow Background Color'/>
+            <div className='header__scores'>
                 <div className='header__scores-current'>0</div>
                 <div className='header__scores-best'>0</div>
-            </article>
+            </div>
+            <InstructionsSection />
         </header>
     );
 }
