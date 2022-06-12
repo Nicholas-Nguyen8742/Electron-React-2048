7abb0b414ff58de6f0dfeb1401bc1481b447d165
Nicholas-Nguyen8742 <NicholasNguyen8742@gmail.com>
2022-06-12 06:18:16 -0400

    Added game tile styling

--- src/components/GameBoard/Gameboard.jsx
+++ src/components/GameBoard/Gameboard.jsx
@@ -1,11 +1,28 @@
 import "./GameBoard.scss";
 import React from "react";
+import GameTile from "../GameTile/GameTile";
 
 export default function Gameboard() {
   return (
     <div className="gamboard-wrapper">
       <section className="gameboard">
-      
+        <GameTile />
+        <GameTile />
+        <GameTile />
+        <GameTile />
+        <GameTile />
+        <GameTile />
+        <GameTile />
+        <GameTile />
+        <GameTile />
+        <GameTile />
+        <GameTile />
+        <GameTile />
+        <GameTile />
+        <GameTile />
+        <GameTile />
+        <GameTile />
+
       </section>
     </div>
   );
---
+++ src/components/GameTile/GameTile.jsx
@@ -0,0 +1,12 @@
+import './GameTile.scss';
+import React from 'react';
+
+function GameTile() {
+    return (
+        <div className='tile'>
+            
+        </div>
+    );
+}
+
+export default GameTile;
