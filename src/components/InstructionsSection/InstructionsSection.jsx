e256f04312eb10933c7c0211dce1a69479e5087d
Nicholas-Nguyen8742 <NicholasNguyen8742@gmail.com>
2022-06-12 03:50:46 -0400

    Basic structure for instructions section

---
+++ src/components/InstructionsSection/InstructionsSection.jsx
@@ -0,0 +1,11 @@
+import './InstructionsSection.scss';
+import React from 'react';
+
+export default function InstructionsSection() {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
