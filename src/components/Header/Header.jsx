9cd144281cd05e503b07d0982bb802b8286ceeb5
Nicholas-Nguyen8742 <NicholasNguyen8742@gmail.com>
2022-06-12 05:41:36 -0400

    Styling for header completed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,26 +1,27 @@
-import './Header.scss';
-import React from 'react';
-import logo from '../../assets/images/2048_logo.png';
-
+import "./Header.scss";
+import React from "react";
+import logo from "../../assets/images/2048_logo.png";
 
 export default function Header() {
-    return (
-        <header className='header'>
-            <img className="header__logo" src={logo} alt='The Number 2048 on Yellow Background Color'/>
-            <div className='header__scores'>
-                <article className='header__scores-container'>
-                    <h4 className='header__scores__title'>Score</h4>
-                    <h3>0</h3>
-                </article>
-                <article className='header__scores-container'>
-                    <h4 className='header__scores__title'>Best</h4>
-                    <h3>0</h3>
-                </article>
-            </div>
-        </header>
-    );
+  return (
+    <header className="header">
+      <img
+        className="header__logo"
+        src={logo}
+        alt="The Number 2048 on Yellow Background Color"
+      />
+      <div className="header__scores">
+        <article className="header__scores-container">
+          <h4 className="header__scores__title">Score</h4>
+          <h3>0</h3>
+        </article>
+        <article className="header__scores-container ">
+          <h4 className="header__scores__title">Best</h4>
+          <h3>0</h3>
+        </article>
+        <button className="header__button-new header__button">NEW</button>
+        <button className="header__button-undo header__button">UNDO</button>
+      </div>
+    </header>
+  );
 }
-// <div className='header__buttons'>
-// <button className='header__buttons-new'>NEW</button>
-// <button className='header__buttons-undo'>UNDO</button>
-// </div>
